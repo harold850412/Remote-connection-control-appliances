d3c85b7976feb9872f8510bf457c6a89f8288306
harold850412 <42542265+harold850412@users.noreply.github.com>
2020-01-06 21:03:31 +0800

    Add files via upload

--- led_breathe.py
+++ led_breathe.py
@@ -1,7 +1,7 @@
 import pigpio
 import time
  
-PWM_LED_PIN = 18
+LED_1 = 18
 PWM_FREQ = 800
  
 pi = pigpio.pi()
@@ -9,12 +9,12 @@ pi = pigpio.pi()
 try:
     while True:
         for i in range(0, 30, 1):
-            pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, i*10000)
+            pi.hardware_PWM(LED_1, PWM_FREQ, i*10000)
             time.sleep(0.1)
         for i in range(29, -1, -1):
-            pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, i*10000)
+            pi.hardware_PWM(LED_1, PWM_FREQ, i*10000)
             time.sleep(0.1)
 except KeyboardInterrupt:
     print('關閉程式')
 finally:
-    pi.set_mode(PWM_LED_PIN, pigpio.INPUT)
+    pi.set_mode(LED_1, pigpio.INPUT)
